eadd60c39ae5e7d9238651fe16988bc9211f424b
Jhonathan C. Queiroz <74057391+jhonathanqz@users.noreply.github.com>
2024-04-11 14:30:41 -0300

    Feat/update interceptor (#8)
    * feat: update interceptor
    
    * feat: update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.0.3
+
+- - Update method print to requests interceptor, add new properties and configure new interceptor
+
+## 1.0.2
+
+- - Update method print to requests interceptor and add new properties
+
 ## 1.0.1
 
 - - Update method print to requests interceptor
--- lib/src/dio_log_main.dart
+++ lib/src/dio_log_main.dart
@@ -1,8 +1,8 @@
-import 'dart:developer';
+// ignore_for_file: avoid_print
 
 import 'package:dio/dio.dart';
 
-class DioLoggerRequest extends Interceptor {
+class DioLoggerRequest extends InterceptorsWrapper {
   @override
   Future<void> onRequest(
     RequestOptions options,
@@ -12,6 +12,7 @@ class DioLoggerRequest extends Interceptor {
 ┌==============================================================================
 | #[DIO] Request
 | => Request: ${options.method} ${options.uri}
+| => Path: ${options.path}
 | => Options: ${options.data.toString()}
 |_______________________________________________________________________________
 | Headers:
@@ -37,6 +38,7 @@ class DioLoggerRequest extends Interceptor {
 ┌==============================================================================
 | #[DIO] Response
 | StatusCode: [${response.statusCode}]
+| StatusMessage: [${response.statusMessage}]
 |_______________________________________________________________________________
 | => Response: ${response.data.toString()}
 └==============================================================================
@@ -54,9 +56,10 @@ class DioLoggerRequest extends Interceptor {
     _logError('''
 ┌===============================================================================
 | #[DIO=LOG] Error
-| => Type: ${err.type}
+| => Type: ${err.type.toString()}
+| => Path: ${err.requestOptions.path}
 |_______________________________________________________________________________
-| => Error: ${err.error}: ${err.response.toString()}
+| => Error: ${err.error.toString()}: ${err.response.toString()}
 |_______________________________________________________________________________
 | => Message: ${err.message}
 └===============================================================================
@@ -65,19 +68,45 @@ class DioLoggerRequest extends Interceptor {
     handler.next(err); //
     return super.onError(err, handler);
   }
-
-  // Green text
-  void _logSuccess(String msg) {
-    print('\x1B[32m$msg\x1B[0m');
-  }
-
-  // Red text
-  void _logError(String msg) {
-    print('\x1B[31m$msg\x1B[0m');
-  }
-
-  // Blue text
-  void _logInfo(String msg) {
-    print('\x1B[34m$msg\x1B[0m');
-  }
 }
+
+// Green text
+void _logSuccess(String msg) {
+  print('\x1B[32m$msg\x1B[0m');
+}
+
+// Red text
+void _logError(String msg) {
+  print('\x1B[31m$msg\x1B[0m');
+}
+
+// Blue text
+void _logInfo(String msg) {
+  print('\x1B[34m$msg\x1B[0m');
+}
+
+final dioLoggerRequestInterceptor = InterceptorsWrapper(
+  onRequest: (RequestOptions options, handler) {
+    _logInfo("┌------------------------------------------------------------------------------");
+    _logInfo('| [DIO] Request: ${options.method} ${options.uri}');
+    _logInfo('| ${options.data.toString()}');
+    _logInfo('| Headers:');
+    options.headers.forEach(
+      (key, value) {
+        _logInfo('|\t$key: $value');
+      },
+    );
+    _logInfo("├------------------------------------------------------------------------------");
+    handler.next(options);
+  },
+  onResponse: (Response response, handler) async {
+    _logSuccess("| [DIO] Response [code ${response.statusCode}]: ${response.data.toString()}");
+    _logSuccess("└------------------------------------------------------------------------------");
+    handler.next(response);
+  },
+  onError: (DioException error, handler) async {
+    _logError("| [DIO] Error: ${error.error}: ${error.response.toString()}");
+    _logError("└------------------------------------------------------------------------------");
+    handler.next(error);
+  },
+);
